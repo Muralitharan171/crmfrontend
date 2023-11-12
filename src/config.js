a62328979c369dc0106e17c104724a6e4f73811c
Muralitharan171 <murran171@gmail.com>
2023-11-12 12:26:31 +0530

    comit

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export const env = {
-     api : "http://localhost:4000"
-   // api : "https://crm-backend-tlp8.onrender.com"
+    // api : "http://localhost:4000"
+    api : "https://crmbackend-aahj.onrender.com/"
 }
